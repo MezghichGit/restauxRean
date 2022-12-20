63233b8604be91012232ebe2a01ef3399fe19350
moahmed amine mezghich <git@mezghich.com>
2022-12-20 13:59:07 +0100

    List Restaux avec api

--- screens/ListRestauxScreen.js
+++ screens/ListRestauxScreen.js
@@ -1,13 +1,77 @@
-import {Text, View, Button} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Image, TextInput, PermissionsAndroid, StyleSheet, Text, View, Button, SafeAreaView, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+
 import styles from '../styles';
 
+
+/***** Composant Flat ListItem */
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+        <Text style={[styles.title, textColor]}>Numéro du restau : {item.id}</Text>
+        <Text style={[styles.title, textColor]}>Nom : {item.nom}</Text>
+        <Text style={[styles.title, textColor]}>Adresse : {item.adresse}</Text>
+        <Text style={[styles.title, textColor]}>Ville : {item.ville}</Text>
+    </TouchableOpacity>
+);
+/******* Fin du FlatList */
+
+
+
  const ListRestauxScreen = ({ navigation }) =>{
-    return (
-        <View style={styles.container}>
-            <Text>List RestauxScreen</Text>
-            <Button title="Détails Restaux" onPress={()=>navigation.navigate('DetailsRestau')}></Button>
-        </View>
+
+    const [selectedId, setSelectedId] = useState(null);
+    const [restaux, setRestaux] = useState([]);
+    const [fetchedState, setFetchedState] = useState(null);
+
+    useEffect(
+        () => {
+            setFetchedState('Loading');
+            setTimeout(() => getData(), 1000);
+        }, []
     );
+    const getData = async () => {
+        //const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const response = await fetch('https://restaux.smart-it-partner.com/public/index.php/api/restaurants');
+        const data = await response.json()
+        setRestaux(data['hydra:member'])
+        setFetchedState(null);
+        console.log(data)
+    }
+
+        /////
+        const renderItem = ({ item }) => {
+            const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+            const color = item.id === selectedId ? 'white' : 'black';
+    
+            return (
+                <Item
+                    item={item}
+                    onPress={() => {
+                        setSelectedId(item.id)
+                        //console.log(item.id)
+                        navigation.navigate('DetailsRestau', { restauId: item.id })
+                    }
+                    }
+                    backgroundColor={{ backgroundColor }}
+                    textColor={{ color }}
+                />
+            );
+        };
+    
+        return (
+    
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.titreText}>List RestauxScreen</Text>
+                    {
+                        fetchedState ? <ActivityIndicator size="large" color="#0000ff" /> :
+                            <FlatList
+                                data={restaux}
+                                renderItem={renderItem}
+                            />
+                    }
+                </SafeAreaView>
+                );
+        
 }
 
 export default ListRestauxScreen;
