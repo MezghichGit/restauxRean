d400d857295ab925b459e664f6aa968290f6a72a
moahmed amine mezghich <git@mezghich.com>
2022-12-20 15:59:40 +0100

    add Demande

--- screens/AddDemandeScreen.js
+++ screens/AddDemandeScreen.js
@@ -1,13 +1,76 @@
-import {Text, View, Button} from 'react-native';
+import {Text, TextInput, Button,SafeAreaView} from 'react-native';
+import React, { useState} from "react";
 import styles from '../styles';
 
  const AddDemandeScreen = ({ navigation }) =>{
+
+    const [nom, setNom] = useState("");
+    const [email, setEmail] = useState("");
+    const [telephone, setTelephone] = useState("");
+    const [adresse, setAdresse] = useState("");
+
+    //const dateDemande = Date.now();
+
+    var currentdate = new Date();
+
+    var datetime =  currentdate.getFullYear() + "-" +(currentdate.getMonth()+1)+"-"+currentdate.getDate()+" "+currentdate.getHours()+":"+currentdate.getMinutes() +":"+currentdate.getSeconds();
+
+
+    let demande = {
+        nom:nom,
+        email:email,
+        telephone:telephone,
+        adresse:adresse,
+        createdAt:datetime
+    }
+
+    const addDemandeData = ()=> {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(demande)
+        };
+        fetch('https://restaux.smart-it-partner.com/public/index.php/api/demandes', requestOptions)
+            .then(response => {
+                response.json();
+                setTimeout(navigation.push('ListDemandes'),3000)
+              })
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Add Demande Screen</Text>
-            <Button title="Liste Demande" onPress={()=>navigation.navigate('ListDemandes')}></Button>
-        </View>
+        <SafeAreaView>
+            <Text style={styles.titreText}>Nouvelle Demande</Text>
+            <Text>Nom : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setNom}
+                value={nom}
+            />
+            <Text>Email: </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setEmail}
+                value={email}
+
+            />
+            <Text>Téléphone : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTelephone}
+                value={telephone}
+
+            />
+            <Text>Adresse : </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setAdresse}
+                value={adresse}
+
+            />
+            <Button title="Ajouter" onPress={addDemandeData}></Button>
+        </SafeAreaView>
     );
 }
+
 
 export default AddDemandeScreen;
--- screens/ListDemandesScreen.js
+++ screens/ListDemandesScreen.js
@@ -1,14 +1,76 @@
-import {Text, View, Button} from 'react-native';
+
+import React, { useState, useEffect } from "react";
+import { Image, TextInput, PermissionsAndroid, StyleSheet, Text, View, Button, SafeAreaView, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+
 import styles from '../styles';
 
+/***** Composant Flat ListItem */
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+        <Text style={[styles.title, textColor]}>id : {item.id}</Text>
+        <Text style={[styles.title, textColor]}>Nom : {item.nom}</Text>
+        <Text style={[styles.title, textColor]}>Email : {item.email}</Text>
+        <Text style={[styles.title, textColor]}>Téléphone: {item.telephone}</Text>
+        <Text style={[styles.title, textColor]}>Adresse: {item.adresse}</Text>
+        <Text style={[styles.title, textColor]}>Date {item.createdAt}</Text>
+    </TouchableOpacity>
+);
+/******* Fin du FlatList */
+
+
  const ListDemandesScreen = ({ navigation }) =>{
-    return (
-        <View style={styles.container}>
-            <Text>List DemandesScreen</Text>
-            <Button title="Add Demande" onPress={()=>navigation.navigate('AddDemande')}></Button>
-            <Button title="Détails Demande" onPress={()=>navigation.navigate('DetailsDemande')}></Button>
-        </View>
+
+    const [selectedId, setSelectedId] = useState(null);
+    const [demandes, setDemandes] = useState([]);
+    const [fetchedState, setFetchedState] = useState(null);
+
+    useEffect(
+        () => {
+            setFetchedState('Loading');
+            setTimeout(() => getData(), 1000);
+        }, []
     );
+    const getData = async () => {
+        //const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const response = await fetch('https://restaux.smart-it-partner.com/public/index.php/api/demandes')
+        const data = await response.json()
+        setDemandes(data['hydra:member'])
+        setFetchedState(null);
+        console.log(data)
+    }
+
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+        const color = item.id === selectedId ? 'white' : 'black';
+
+        return (
+            <Item
+                item={item}
+                onPress={() => {
+                    setSelectedId(item.id)
+                    navigation.navigate('DetailsDemande', { demandeId: item.id })
+                }
+                }
+                backgroundColor={{ backgroundColor }}
+                textColor={{ color }}
+            />
+        );
+    };
+    return (
+        <SafeAreaView style={styles.container}>
+           
+        <Text style={styles.titreText}>List Demandes</Text>
+            {
+                fetchedState ? <ActivityIndicator size="large" color="#0000ff" /> :
+                    <FlatList
+                        data={demandes}
+                        renderItem={renderItem}
+                    />
+            }
+
+<Button title="Add Demande" onPress={()=>navigation.navigate('AddDemande')}></Button>
+        </SafeAreaView>
+        );
 }
 
 export default ListDemandesScreen;
